eac2595f321d678c44f4a1c0a91f09b0846979db
kralle3 <kamarirallen@gmail.com>
2025-01-30 15:24:59 -0600

    Ultrasonic distance tester
    added distance tester code

--- sketch_jan30a/sketch_jan30a.ino
+++ sketch_jan30a/sketch_jan30a.ino
@@ -6,9 +6,14 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);
+long duration, distance;
+
 void setup() {
   // put your setup code here, to run once:
 AFMS.begin();
+pinMode(7, OUTPUT);
+pinMode(8, INPUT);
+Serial.begin(9600); 
 }
 
 void loop() {
@@ -53,4 +58,16 @@ leftMotor->setSpeed(0);
 
 rightMotor->run(FORWARD);
 leftMotor->run(FORWARD);
+}
+
+long distanceTest() {
+digitalWrite(7, LOW);
+delayMicroseconds(2);
+digitalWrite(7, HIGH);
+delayMicroseconds(10);
+digitalWrite(7, LOW);
+duration = pulseIn(8, HIGH);
+distance = (duration/2) / 29.1;
+Serial.print(distance);
+return distance;
 }
